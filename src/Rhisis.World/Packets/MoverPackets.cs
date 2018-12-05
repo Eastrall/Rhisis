a57b96a3f3acd34128cf6544ba6571bea5ce65d7
Kaev <Demonhunter@hotmail.de>
2018-12-05 20:48:44 +0100

    Revert WasdMovement changes

--- src/Rhisis.World/Packets/MoverPackets.cs
+++ src/Rhisis.World/Packets/MoverPackets.cs
@@ -1,5 +1,4 @@
-﻿using Rhisis.Core.Structures;
-using Rhisis.Network;
+﻿using Rhisis.Network;
 using Rhisis.Network.Packets;
 using Rhisis.World.Game.Core;
 
@@ -14,31 +13,6 @@ namespace Rhisis.World.Packets
                 packet.StartNewMergedPacket(entity.Id, SnapshotType.SET_SPEED_FACTOR);
                 packet.Write(speedFactor);
 
-                SendToVisible(packet, entity);
-            }
-        }
-
-        public static void SendMoverMoved(IEntity entity, Vector3 beginPosition, Vector3 destinationPosition,
-            float speedFactor, uint state, uint stateFlag, uint motion, int motionEx, int loop, uint motionOption,
-            long tickCount)
-        {
-            using (var packet = new FFPacket())
-            {
-                packet.StartNewMergedPacket(entity.Id, SnapshotType.MOVERMOVED);
-                packet.Write(beginPosition.X);
-                packet.Write(beginPosition.Y);
-                packet.Write(beginPosition.Z);
-                packet.Write(destinationPosition.Z);
-                packet.Write(destinationPosition.Y);
-                packet.Write(destinationPosition.Z);
-                packet.Write(speedFactor);
-                packet.Write(state);
-                packet.Write(stateFlag);
-                packet.Write(motion);
-                packet.Write(motionEx);
-                packet.Write(loop);
-                packet.Write(motionOption);
-                packet.Write(tickCount);
                 SendToVisible(packet, entity);
             }
         }
