f3bb79dfc004d86475039d9d178b70c59ac34f8c
Freezeraid <kilanju@outlook.fr>
2019-11-13 22:41:48 +0100

    Revert "Begin of Npc BUFF system"
    This reverts commit a7820a149d15e7925a10bb62b1f264dc1be74f6c.

--- src/Rhisis.World/Systems/NpcBuff/INpcBuffSystem.cs
+++
@@ -1,9 +0,0 @@
-﻿using Rhisis.World.Game.Entities;
-
-namespace Rhisis.World.Systems.NpcBuff
-{
-    public interface INpcBuffSystem
-    {
-        void NpcBuff(IPlayerEntity player, string buff);
-    }
-}
