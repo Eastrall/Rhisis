a7820a149d15e7925a10bb62b1f264dc1be74f6c
Freezeraid <kilanju@outlook.fr>
2019-11-13 18:59:34 +0100

    Begin of Npc BUFF system

---
+++ src/Rhisis.Network/Packets/World/NpcBuffPacket.cs
@@ -0,0 +1,21 @@
+﻿using Sylver.Network.Data;
+
+namespace Rhisis.Network.Packets.World
+{
+    /// <summary>
+    /// Defines the <see cref="NpcBuffPacket"/> packet structure.
+    /// </summary>
+    public class NpcBuffPacket : IPacketDeserializer
+    {
+        /// <summary>
+        /// Gets the selected buff string.
+        /// </summary>
+        public string buff { get; set; }
+
+        /// <inheritdoc />
+        public void Deserialize(INetPacketStream packet)
+        {
+            this.buff = packet.Read<string>();
+        }
+    }
+}
