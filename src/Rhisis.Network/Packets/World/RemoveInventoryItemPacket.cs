00c6d25fe013c4d8031266fdd30f354e9ed0ef89
Freezeraid <kilanju@outlook.fr>
2019-11-13 22:41:39 +0100

    Able to delete item from inventory

--- src/Rhisis.Network/Packets/World/RemoveInventoryItemPacket.cs
+++ src/Rhisis.Network/Packets/World/RemoveInventoryItemPacket.cs
@@ -6,26 +6,26 @@ namespace Rhisis.Network.Packets.World
     /// <summary>
     /// Defines the <see cref="RemoveInventoryItemPacket"/> structure.
     /// </summary>
-    public struct RemoveInventoryItemPacket : IEquatable<RemoveInventoryItemPacket>
+    public class RemoveInventoryItemPacket : IPacketDeserializer
     {
         /// <summary>
-        /// Gets the item id.
+        /// Gets the unique item id.
         /// </summary>
-        public uint ItemId { get; set; }
+        public int ItemUniqueId { get; private set; }
 
         /// <summary>
-        /// Gets the quantity.
+        /// Gets the item quantity.
         /// </summary>
-        public int Quantity { get; set; }
+        public int ItemQuantity { get; private set; }
 
         /// <summary>
         /// Creates a new <see cref="RemoveInventoryItemPacket"/> object.
         /// </summary>
         /// <param name="packet">Incoming packet</param>
-        public RemoveInventoryItemPacket(INetPacketStream packet)
+        public void Deserialize(INetPacketStream packet)
         {
-            this.ItemId = packet.Read<uint>();
-            this.Quantity = packet.Read<int>();
+            this.ItemUniqueId = packet.Read<int>();
+            this.ItemQuantity = packet.Read<int>();
         }
 
         /// <summary>
@@ -34,7 +34,7 @@ namespace Rhisis.Network.Packets.World
         /// <param name="other">Other <see cref="RemoveInventoryItemPacket"/></param>
         public bool Equals(RemoveInventoryItemPacket other)
         {
-            return this.ItemId == other.ItemId && this.Quantity == other.Quantity;
+            return this.ItemUniqueId == other.ItemUniqueId && this.ItemQuantity == other.ItemQuantity;
         }
     }
 }
