0e17c7c35fd28cfdb9e25e1792eb3728e081fff3
Freezeraid <kilanju@outlook.fr>
2019-11-13 18:59:34 +0100

    Begin of Npc BUFF system

---
+++ src/Rhisis.World/Systems/NpcBuff/INpcBuffSystem.cs
@@ -0,0 +1,9 @@
+﻿using Rhisis.World.Game.Entities;
+
+namespace Rhisis.World.Systems.NpcBuff
+{
+    public interface INpcBuffSystem
+    {
+        void NpcBuff(IPlayerEntity player, string buff);
+    }
+}
